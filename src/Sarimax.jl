8abd5e10bbb8c3c93756b121f1860963687ecaf9
LuizFDuarte <luizduarte@lampsco.com>
2023-09-07 18:06:49 -0300

    :sparkles: Add datasets methods

--- src/Sarimax.jl
+++ src/Sarimax.jl
@@ -11,17 +11,22 @@ using LinearAlgebra
 using Statistics
 using OffsetArrays
 using Distributions
+using DataFrames
+using CSV
 # using GLMNet
 # using Lasso
 
 abstract type SarimaxModel end
 
 include("datasets.jl")
+include("utils.jl")
 include("models/sarima.jl")
 
 # Export types
 export SARIMAModel
 
+# Export enums
+export Datasets
 
 # Export functions
 export splitTrainTest
@@ -33,6 +38,7 @@ export SARIMA
 export differentiate
 export integrate
 export simulate
+export loadDataset
 
 
 end # module Sarimax
---
+++ src/datasets.jl
@@ -0,0 +1,84 @@
+export AIR_PASSENGERS
+
+@enum Datasets begin
+    AIR_PASSENGERS=1
+end
+
+datasetsPaths = [
+    joinpath(dirname(@__DIR__()), "datasets", "airpassengers.csv")
+]
+
+"""
+    loadDataset(
+        dataset::Datasets
+    )
+
+Loads a dataset from the `Datasets` enum. 
+
+# Example
+```jldoctest
+julia> airPassengers = loadDataset(AIR_PASSENGERS)
+204×1 TimeArray{Float64, 1, Date, Vector{Float64}} 1991-07-01 to 2008-06-01
+│            │ value   │
+├────────────┼─────────┤
+│ 1991-07-01 │ 3.5266  │
+│ 1991-08-01 │ 3.1809  │
+│ ⋮          │ ⋮       │
+│ 2008-06-01 │ 19.4317 │
+
+```
+"""
+function loadDataset(dataset::Datasets)
+    datasetIndex = Integer(dataset)
+    seriesData = CSV.read(datasetsPaths[datasetIndex], DataFrame)
+    y = TimeArray(seriesData, timestamp = :date)
+    return y
+end
+
+"""
+    loadDataset(
+        df::DataFrame
+    )
+
+Loads a dataset from a Dataframe. If the DataFrame does not have a column named
+`date` a new column will be created with the index of the DataFrame. 
+
+# Example
+```jldoctest
+julia> airPassengersDf = CSV.File("datasets/airpassengers.csv") |> DataFrame
+julia> airPassengers = loadDataset(airPassengersDf)
+204×1 TimeArray{Float64, 1, Date, Vector{Float64}} 1991-07-01 to 2008-06-01
+│            │ value   │
+├────────────┼─────────┤
+│ 1991-07-01 │ 3.5266  │
+│ 1991-08-01 │ 3.1809  │
+│ ⋮          │ ⋮       │
+│ 2008-06-01 │ 19.4317 │
+
+```
+"""
+function loadDataset(df::DataFrame)
+    auxiliar_df = deepcopy(df)
+    if !(:date in propertynames(auxiliar_df))
+        @warning("The DataFrame does not have a column named 'date'.")
+        @warnig("Creating a date column with the index of the DataFrame")
+        auxiliar_df[!,:date] = [Date(i) for i=1:size(auxiliar_df,1)]
+    end
+    y = TimeArray(auxiliar_df, timestamp = :date)
+    return y
+end
+
+"""
+    splitTrainTest(
+        data::TimeArray;
+        trainSize::Float64=0.8
+    )
+
+Splits the time series in training and testing sets. 
+"""
+function splitTrainTest(data::TimeArray; trainPercentage::Float64=0.8)
+    trainingSetEndIndex = floor(Int64, trainPercentage*length(y))
+    trainingSet = TimeArray(timestamp(data)[1:trainingSetEndIndex], values(data)[1:trainingSetEndIndex])
+    testingSet = TimeArray(timestamp(data)[trainingSetEndIndex+1:end], values(data)[trainingSetEndIndex+1:end])
+    return trainingSet, testingSet
+end
