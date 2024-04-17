05c94330f57db4342d6273f71acd2e8f73bc6cb0
LuizFCDuarte <luizduarte@aluno.puc-rio.com.br>
2024-04-17 14:10:10 -0300

    :memo: Document utils functions

--- src/utils.jl
+++ src/utils.jl
@@ -6,9 +6,19 @@
         s::Int=1
     )
 
-    Differentiates a TimeArray `series` `d` times and `D` times with a seasonal difference of `s` periods.
+Differentiates a `TimeArray` `series` `d` times and `D` times with a seasonal difference of `s` periods.
 
-    This method only works with d,D ∈ {0,1}.
+# Arguments
+- `series::TimeArray`: The time series data to differentiate.
+- `d::Int=0`: The number of non-seasonal differences to take.
+- `D::Int=0`: The number of seasonal differences to take.
+- `s::Int=1`: The seasonal period for the differences.
+
+# Returns
+A differentiated `TimeArray`.
+
+# Errors
+- This method only works with `d` and `D` in the set {0,1}.
 
 # Example
 ```jldoctest
@@ -46,49 +56,6 @@ function differentiate(series::TimeArray,d::Int=0, D::Int=0, s::Int=1)
         series = TimeArray(copy(timestamp(series))[2:end],diffValues,[seriesName])
     end
     return series
-end
-
-"""
-    integrate(
-        series::TimeArray, 
-        diffSeries::Vector{Fl}, 
-        d::Int = 0, 
-        D::Int = 0, 
-        s::Int = 1
-    ) where Fl<:Real
-
-    Integrates a vector of a differentiated series `diffSeries` `d` times and `D` times with a seasonal difference of `s` periods assuming the original previous observations are in `series`.
-
-    This method only works with d,D ∈ {0,1}.
-"""
-function integrate(series::TimeArray, diffSeries::Vector{Fl}, d::Int=0, D::Int=0, s::Int=1) where Fl<:Real
-    series = TimeArray(timestamp(series),values(series))
-    stepsAhead = length(diffSeries)
-    y = deepcopy(values(series))
-    T = length(y)
-    y = vcat(y,diffSeries)
-    for i=T+1:T+stepsAhead
-        # @info("Non seasonal integration")
-        recoveredValue = y[i]
-        # Δyt = y[t] - y[t-1] - y[t-12] + y[t-12-1]
-        for _ in 1:d
-            # Δyₜ = yₜ - y_t-1 ⇒ yₜ = Δyₜ + y_t-1
-            recoveredValue += y[i-1]
-        end
-        # @info("Seasonal integration")
-        for _ in 1:D
-            # Δyₜ = yₜ - y_t-s ⇒ yₜ = Δyₜ + y_t-s
-            recoveredValue += y[i-s]
-        end
-        # @info("Correction for seasonal integration")
-        if D > 0 && d > 0
-            # Δyₜ = yₜ - y_t-s ⇒ yₜ = Δyₜ + y_t-s
-            recoveredValue -= y[i-s-1]
-        end
-        y[i] = recoveredValue
-    end
-    
-    return y[T+1:end]
 end
 
 """
@@ -291,12 +258,25 @@ end
 
 
 """
-    loglikelihood(
-        model::SARIMAModel
-    )
+    loglikelihood(model::SarimaxModel)
 
-    Calculates the loglikelihood of a SARIMA model based on the following formula:
-    -0.5 * (T * log(2π) + T * log(σ²) + sum(ϵ.^2 ./ σ²))
+Calculate the log-likelihood of a SARIMAModel.
+The log-likelihood is calculated based on the formula
+`-0.5 * (T * log(2π) + T * log(σ²) + sum(ϵ.^2 ./ σ²))`
+where:
+- T is the length of the residuals vector (ϵ).
+- σ² is the estimated variance of the model.
+
+# Arguments
+- `model::SarimaxModel`: A SARIMAModel object.
+
+# Returns
+- The log-likelihood of the SARIMAModel.
+
+# Errors
+- `MissingMethodImplementation("fit!")`: Thrown if the `fit!` method is not implemented for the given model type.
+- `ModelNotFitted()`: Thrown if the model has not been fitted.
+
 """
 function loglikelihood(model::SarimaxModel)
     !hasFitMethods(typeof(model)) && throw(MissingMethodImplementation("fit!"))
@@ -306,20 +286,21 @@ function loglikelihood(model::SarimaxModel)
 end
 
 """
-    loglike(
-        model::SARIMAModel
-    )
+    loglike(model::SarimaxModel)
 
-    For a set of independent data points {x_1, x_2, ..., x_n} assumed to be drawn from a probability distribution with a PDF f(x; θ), where θ represents the model parameters, the log-likelihood LL(θ) is calculated as the sum of the log PDFs:
+Calculate the log-likelihood of a SARIMAModel using the normal probability density function.
+The log-likelihood is calculated by summing the logarithm of the probability density function (PDF) of each data point under the assumption of a normal distribution with mean 0 and standard deviation equal to the square root of the estimated variance (σ²) of the model.
 
-    LL(θ) = Σ[log(f(x_i; θ))]
+# Arguments
+- `model::SarimaxModel`: A SARIMAModel object.
 
-    Here:
+# Returns
+- The log-likelihood of the SARIMAModel.
 
-    LL(θ) is the log-likelihood.
-    Σ denotes the summation over all data points from i = 1 to n.
-    log() represents the natural logarithm.
-    f(x_i; θ) is the probability density function of the model evaluated at data point x_i with parameter θ.
+# Errors
+- `MissingMethodImplementation("fit!")`: Thrown if the `fit!` method is not implemented for the given model type.
+- `ModelNotFitted()`: Thrown if the model has not been fitted.
+
 """
 function loglike(model::SarimaxModel)
     !hasFitMethods(typeof(model)) && throw(MissingMethodImplementation("fit!"))
