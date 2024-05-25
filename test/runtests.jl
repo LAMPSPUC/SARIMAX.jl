c388db0be33b89df5b318b9529d1338a1b84d858
LuizFCDuarte <luizduarte@aluno.puc-rio.com.br>
2024-05-25 15:57:08 -0300

    :bug: Fix path to test files in runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Statistics
 using Test
 
 # Testes dos modelos
-include(joinpath(BASE_PATH,"test/models/sarima.jl"))
+include("models/sarima.jl")
 
-include(joinpath(BASE_PATH,"test/datetime_utils.jl"))
+include("datetime_utils.jl")
 
-include(joinpath(BASE_PATH,"test/utils.jl"))
+include("utils.jl")
