4c095502d3ef83c6c4e7d688f4a67a287b28e682
LuizFCDuarte <luizduarte@aluno.puc-rio.com.br>
2024-06-01 12:42:07 -0300

    :art: Improve predict testsets format

--- test/models/sarima_predict.jl
+++ test/models/sarima_predict.jl
@@ -56,55 +56,56 @@ end
 function MAE(actual, forecast)
     return values(mean(abs.(actual .- forecast)))[1]
 end
+@testset "Sarima predict" begin 
+    @testset "predict sarima without white noise" begin
+        #p=2 P=1 trend =0.1
+        ARcoeff = [-0.3,-0.2]
+        seasCoeff = 0.4
+        trend = 0.1
+        ARseries = generateARseries(2,12,ARcoeff, seasCoeff,trend,1234,false)
+        trainingSet, testingSet = splitTrainTest(ARseries)
+        modelMSE = SARIMA(trainingSet,2,1,0;seasonality =12,P=1,D=0,Q=0)
+        Sarimax.fit!(modelMSE)
+        print(modelMSE)
+        forecastMSE= Sarimax.predict!(modelMSE; stepsAhead = length(testingSet))
+        maeMSE = MAE(testingSet,forecastMSE)
+        mapeMSE = MAPE(testingSet,forecastMSE)
+        @test maeMSE ≈ 0 atol = 1e-3
+        @test mapeMSE ≈ 0 atol = 1e-3
 
-@testset "predict sarima without white noise" begin
-    #p=2 P=1 trend =0.1
-    ARcoeff = [-0.3,-0.2]
-    seasCoeff = 0.4
-    trend = 0.1
-    ARseries = generateARseries(2,12,ARcoeff, seasCoeff,trend,1234,false)
-    trainingSet, testingSet = splitTrainTest(ARseries)
-    modelMSE = SARIMA(trainingSet,2,1,0;seasonality =12,P=1,D=0,Q=0)
-    Sarimax.fit!(modelMSE)
-    print(modelMSE)
-    forecastMSE= Sarimax.predict!(modelMSE; stepsAhead = length(testingSet))
-    maeMSE = MAE(testingSet,forecastMSE)
-    mapeMSE = MAPE(testingSet,forecastMSE)
-    @test maeMSE ≈ 0 atol = 1e-3
-    @test mapeMSE ≈ 0 atol = 1e-3
+        #sin
+        seriesSin = generateSeries(0,12,0,0,1234,false)
+        trainingSin, testingSin = splitTrainTest(seriesSin)
+        modelSin = SARIMA(trainingSin,0,0,0;seasonality=12,P=1,D=0,Q=0)
+        Sarimax.fit!(modelSin)
+        print(modelSin)
+        forecastSin = Sarimax.predict!(modelSin; stepsAhead = length(testingSet))
+        maeSin = MAE(testingSin, forecastSin)
+        @test maeSin ≈ 0 atol = 1e-3
+    end 
 
-    #sin
-    seriesSin = generateSeries(0,12,0,0,1234,false)
-    trainingSin, testingSin = splitTrainTest(seriesSin)
-    modelSin = SARIMA(trainingSin,0,0,0;seasonality=12,P=1,D=0,Q=0)
-    Sarimax.fit!(modelSin)
-    print(modelSin)
-    forecastSin = Sarimax.predict!(modelSin; stepsAhead = length(testingSet))
-    maeSin = MAE(testingSin, forecastSin)
-    @test maeSin ≈ 0 atol = 1e-3
-end 
+    @testset "auto predict without white noise" begin
+        #p=2 P=1 trend =1
+        ARcoeff = [0.3,0.3]
+        seasCoeff = 0.5
+        trend = 0.1
+        ARseries = generateARseries(2,12,ARcoeff, seasCoeff,trend,1234,false)
+        trainingSet, testingSet = splitTrainTest(ARseries)
+        modelAuto = Sarimax.auto(trainingSet;seasonality=12,objectiveFunction ="mse", allowMean=false,allowDrift=true)
+        forecastAuto = Sarimax.predict!(modelAuto;stepsAhead=length(testingSet))
+        mapeAuto = MAPE(testingSet,forecastAuto)
+        maeAuto = MAE(testingSet,forecastAuto)
+        @test mapeAuto ≈ 0 atol = 1e-3  
+        @test maeAuto ≈ 0 atol = 1e-3
 
-@testset "auto predict without white noise" begin
-    #p=2 P=1 trend =1
-    ARcoeff = [0.3,0.3]
-    seasCoeff = 0.5
-    trend = 0.1
-    ARseries = generateARseries(2,12,ARcoeff, seasCoeff,trend,1234,false)
-    trainingSet, testingSet = splitTrainTest(ARseries)
-    modelAuto = Sarimax.auto(trainingSet;seasonality=12,objectiveFunction ="mse", allowMean=false,allowDrift=true)
-    forecastAuto = Sarimax.predict!(modelAuto;stepsAhead=length(testingSet))
-    mapeAuto = MAPE(testingSet,forecastAuto)
-    maeAuto = MAE(testingSet,forecastAuto)
-    @test mapeAuto ≈ 0 atol = 1e-3  
-    @test maeAuto ≈ 0 atol = 1e-3
-
-    #p=2 sin seasonality trend=0.1
-    seriesARSeas= generateSeries(2,12,[0.3,0.2],0.1,1234, false)
-    trainingARSeas, testingARSeas = splitTrainTest(seriesARSeas)
-    modelARSeasAuto = Sarimax.auto(trainingARSeas;seasonality=12, objectiveFunction="mse")
-    forecastARSeasAuto = Sarimax.predict!(modelARSeasAuto;stepsAhead=40)
-    mapeARSeasAuto = MAPE(testingARSeas, forecastARSeasAuto)
-    maeARSeasAuto = MAE(testingARSeas, forecastARSeasAuto)
-    @test mapeARSeasAuto ≈ 0 atol = 1e-3  
-    @test maeARSeasAuto ≈ 0 atol = 1e-3
+        #p=2 sin seasonality trend=0.1
+        seriesARSeas= generateSeries(2,12,[0.3,0.2],0.1,1234, false)
+        trainingARSeas, testingARSeas = splitTrainTest(seriesARSeas)
+        modelARSeasAuto = Sarimax.auto(trainingARSeas;seasonality=12, objectiveFunction="mse")
+        forecastARSeasAuto = Sarimax.predict!(modelARSeasAuto;stepsAhead=40)
+        mapeARSeasAuto = MAPE(testingARSeas, forecastARSeasAuto)
+        maeARSeasAuto = MAE(testingARSeas, forecastARSeasAuto)
+        @test mapeARSeasAuto ≈ 0 atol = 1e-3  
+        @test maeARSeasAuto ≈ 0 atol = 1e-3
+    end
 end
